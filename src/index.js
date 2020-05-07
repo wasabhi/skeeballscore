a1d4c3d60dc997a79ecbee22807f21d89383694c
wasabhi <abhinav.keswani@trineo.co.nz>
2020-05-07 09:22:26 -0600

    Basic Skee Ball Score App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Button(props) {
+  return (
+    <button onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+
+class Score extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      score: 0,
+      timer: 60,
+    };
+  }
+
+  handleClick(i) {
+    this.setState({
+      score: this.state.score + i
+    });
+  }
+
+  startNewGame() {
+    this.resetGame()
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  resetGame() {
+    this.setState({
+      score: 0,
+      timer: 60
+    });
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    if (this.state.timer === 0) {
+      clearInterval(this.timerID)
+    } else {
+      this.setState({
+        timer: this.state.timer - 1
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Skee Ball Score App</h1>
+          <h1>Time left: {this.state.timer}</h1>
+          <h1>Current Score: {this.state.score}</h1>
+        </div>
+        <div>
+          <Button value="10 points" onClick={() => this.handleClick(10)} />
+          <Button value="50 points" onClick={() => this.handleClick(50)} />
+          <Button value="100 points" onClick={() => this.handleClick(100)} />
+          <Button value="Start New Game" onClick={() => this.startNewGame()} />
+          <Button value="Reset Game" onClick={() => this.resetGame()} />
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Score />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
